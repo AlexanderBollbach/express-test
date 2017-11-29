a5d06b53525cbe661981eb97d7b0a87505a6fa4f
alexbollbach <alexander.bollbach@1stdibs.com>
2017-11-28 20:41:35 -0500

    save

--- index.js
+++ index.js
@@ -1,24 +1,58 @@
-var express = require('express');
+var express = require("express");
 var app = express();
-
+var path = require("path");
 
 // app.get('/demos/:name', (req, res) => {
 // 	const { name } = req.params
 // 	res.sendFile(__dirname + `/demos/${name}/`)
 // })
 
-app.use(express.static('./dist'));
 
-var server = app.listen(8080, function () {
-   var {host, port} = server.address()
-   console.log(`listening at http://${host}:${port}`)
+
+// var filePath = path.join(__dirname, 'relative/path/to/file');
+
+// app.get("*",  (req, res) => {
+// 	res.sendFile(path.join(__dirname, '/dist/index.html'))
+// })
+
+// app.use(express.static(path.join(__dirname, '/dist'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// latest
+
+app.all("*", (req, res, next) => {
+	console.log(" a request ")
+	console.log(req.url)
+	next()
 })
 
-app.get("*",  (req, res) => {
-	res.sendFile(__dirname + "/dist/index.html")
-})
+// for some urls
+app.use("/demos", (req, res, next) => {
+	console.log(req.url);
+	res.sendFile(path.join(__dirname, `/dist/demos${req.url}index.html`))
+});
 
 
+app.use("/", express.static(path.join(__dirname, 'dist')))
 
+// for all others
+// app.use((req, res, next) => res.sendFile(path.join(__dirname, "dist/index.html")));
 
-
+var server = app.listen(8080, function() {
+	var { host, port } = server.address(); console.log(`listening at http://${host}:${port}`);
+});
